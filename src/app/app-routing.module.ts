440537d76c6795e6a7c2aefec492852f28bb01de
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2023-09-09 16:21:36 -0500

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaClientesComponent } from './lista-clientes/lista-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "clientes", component:ListaClientesComponent},
+  {path : '', redirectTo:'clientes', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
